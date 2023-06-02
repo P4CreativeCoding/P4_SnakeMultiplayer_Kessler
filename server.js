5b170b3b6dc8ebe87d094a058d3911a6d9a8a29b
Batmania <ikessler116@gmail.com>
2023-06-02 22:03:06 +0200

    Major changes to the code

--- server.js
+++ server.js
@@ -16,3 +16,34 @@
 // app.listen(port, () => {
 //   console.log(`Server is running on http://localhost:${port}`);
 // });
+
+const express = require('express');
+const app = express();
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer);
+
+const port = 3000;
+
+app.use(express.static('public'));
+
+// Handle client connections
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Handle messages from the client
+  socket.on('message', (data) => {
+    console.log('Received message:', data);
+
+    // Broadcast the message to all connected clients
+    io.emit('message', data);
+  });
+
+  // Handle disconnections
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+httpServer.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
