5f35447d9c8667802b0a647274e1867f42510f9a
Batmania <ikessler116@gmail.com>
2023-06-04 20:50:15 +0200

    test client gameplay 1

--- public/index.js
+++ public/index.js
@@ -1,10 +1,42 @@
-import { SnakeGameClient } from "../public/SnakeGameClient.js";
+var  socket = io();
 
-function startClient(){
-    var  socket = io();
+const gridScale = 20;
 
-    const client = new SnakeGameClient(socket);
-    client.log();
+var players = [];
+socket.on("updatePlayers", (players) => {
+    console.log("Updated players: " + players);
+});
+
+socket.on("updateFood", (data) => {
+    console.log("Updated food.");
+});
+
+function draw(){
+    background(50, 50, 50);
+
+    for(var player in players){
+        if(!player) { continue; }
+
+        fill(0, 200, 0);
+        rect(player.x, player.y, gridScale, gridScale, 5);
+
+        for(var tailPiece in player.tail){
+            rect(tailPiece.x, tailPiece.y, gridScale, gridScale, 5);
+        }
+    }
 };
 
-startClient();
+function keyPressed(){
+    if(UP_ARROW === keyCode || 'w' === keyCode){
+        socket.emit("changeMoveDirection", "up");
+    }
+    else if(DOWN_ARROW === keyCode || 's' === keyCode){
+        socket.emit("changeMoveDirection", "down");
+    }
+    else if(LEFT_ARROW === keyCode || 'a' === keyCode){
+        socket.emit("changeMoveDirection", "left");
+    }
+    else if(RIGHT_ARROW === keyCode || 'd' === keyCode){
+        socket.emit("changeMoveDirection", "right");
+    }
+};
--- server.js
+++ server.js
@@ -2,27 +2,55 @@ const express = require('express');
 const http = require('http');
 const SocketIO = require('socket.io');
 const port = 3000;
+const app = express();
+const server = http.createServer(app);
+const io = SocketIO(server);
 
-function startServer(){
-  const app = express();
-  const server = http.createServer(app);
-  const io = SocketIO(server);
+var players = [];
 
-  server.listen(port, () => {
-    console.log('Example app listening on port' + port);
+function createNewPlayer(socket){
+  players.push({
+    id: socket.id,
+    x: 0,
+    y: 0,
+    tail: []
   });
-
-  app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/index.html")
-    console.log("get()");
-  });
-
-  app.use(express.static("public"));
-
-  const SnakeGameServer = require("./public/SnakeGameServer");
-  const SGS = new SnakeGameServer(app, server, io);
-
-  console.log("Started Snake Game Server!");
 };
 
-startServer();
+function deletePlayer(){
+  for(var i = this.players.length - 1; i >= 0; i--){
+    if(players[i].id == socket.id){
+        players.slice(i, 1);
+    }
+  }
+};
+
+function updatePlayers(){
+  io.emit("updatePlayers", players);
+};
+
+var updatePlayerIntervalID = setInterval(() => {
+  updatePlayers();
+}, 1000);
+
+app.get('/', (res) => {
+  res.sendFile(__dirname + "/index.html")
+});
+
+app.use(express.static("public"));
+
+onClientConnected = function(socket) {
+    createNewPlayer();
+
+    socket.on("disconnect", () => {
+      deletePlayer(socket);
+    });
+};
+
+io.on("connection", (socket) => {
+    onClientConnected(socket);
+});
+
+server.listen(port, () => {
+    console.log('Example app listening on port' + port);
+});
