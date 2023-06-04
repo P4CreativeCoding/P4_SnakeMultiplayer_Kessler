48813fd603f25919910a93cc306fa04fec027f78
Batmania <ikessler116@gmail.com>
2023-06-04 16:41:31 +0200

    test client 2

--- server.js
+++ server.js
@@ -24,27 +24,3 @@ function startServer(){
 };
 
 startServer();
-
-// app.use(express.static('public'));
-
-// // Handle client connections
-// io.on('connection', (socket) => {
-//   console.log('A user connected');
-
-//   // Handle messages from the client
-//   socket.on('message', (data) => {
-//     console.log('Received message:', data);
-
-//     // Broadcast the message to all connected clients
-//     io.emit('message', data);
-//   });
-
-//   // Handle disconnections
-//   socket.on('disconnect', () => {
-//     console.log('A user disconnected');
-//   });
-// });
-
-// httpServer.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// });
