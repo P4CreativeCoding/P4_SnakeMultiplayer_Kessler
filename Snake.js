b34e53739ee95c636a1bf741bfcac38675ed9372
Batmania <ikessler116@gmail.com>
2023-06-04 12:20:00 +0200

    go back to basic

--- Snake.js
+++ Snake.js
@@ -1,90 +1,90 @@
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+// const express = require('express');
+// const app = express();
+// const server = require('http').Server(app);
+// const io = require('socket.io')(server);
 
-const port = 3000;
-const gridSize = 20;
-const width = 800;
-const height = 600;
+// const port = 3000;
+// const gridSize = 20;
+// const width = 800;
+// const height = 600;
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
 
-server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// server.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
 
-const players = {};
+// const players = {};
 
-io.on('connection', (socket) => {
-  console.log('New player connected');
+// io.on('connection', (socket) => {
+//   console.log('New player connected');
 
-  // Create a new player
-  players[socket.id] = {
-    x: Math.floor(Math.random() * gridSize) * gridSize,
-    y: Math.floor(Math.random() * gridSize) * gridSize,
-    direction: 'right',
-    score: 0,
-  };
+//   // Create a new player
+//   players[socket.id] = {
+//     x: Math.floor(Math.random() * gridSize) * gridSize,
+//     y: Math.floor(Math.random() * gridSize) * gridSize,
+//     direction: 'right',
+//     score: 0,
+//   };
 
-  // Send the initial game state to the new player
-  socket.emit('initialState', { players, gridSize, width, height });
+//   // Send the initial game state to the new player
+//   socket.emit('initialState', { players, gridSize, width, height });
 
-  // Update the game state when a player moves
-  socket.on('move', (direction) => {
-    const player = players[socket.id];
-    player.direction = direction;
-  });
+//   // Update the game state when a player moves
+//   socket.on('move', (direction) => {
+//     const player = players[socket.id];
+//     player.direction = direction;
+//   });
 
-  // Remove the player when they disconnect
-  socket.on('disconnect', () => {
-    console.log('Player disconnected');
-    delete players[socket.id];
-  });
-});
+//   // Remove the player when they disconnect
+//   socket.on('disconnect', () => {
+//     console.log('Player disconnected');
+//     delete players[socket.id];
+//   });
+// });
 
 // Update the game state and send it to all players
-setInterval(() => {
-  for (const playerId in players) {
-    const player = players[playerId];
+// setInterval(() => {
+//   for (const playerId in players) {
+//     const player = players[playerId];
 
-    // Move the player
-    if (player.direction === 'up') player.y -= gridSize;
-    else if (player.direction === 'down') player.y += gridSize;
-    else if (player.direction === 'left') player.x -= gridSize;
-    else if (player.direction === 'right') player.x += gridSize;
+//     // Move the player
+//     if (player.direction === 'up') player.y -= gridSize;
+//     else if (player.direction === 'down') player.y += gridSize;
+//     else if (player.direction === 'left') player.x -= gridSize;
+//     else if (player.direction === 'right') player.x += gridSize;
 
-    // Check for collision with the boundaries
-    if (
-      player.x < 0 ||
-      player.x >= width ||
-      player.y < 0 ||
-      player.y >= height
-    ) {
-      // Game over
-      delete players[playerId];
-      continue;
-    }
+//     // Check for collision with the boundaries
+//     if (
+//       player.x < 0 ||
+//       player.x >= width ||
+//       player.y < 0 ||
+//       player.y >= height
+//     ) {
+//       // Game over
+//       delete players[playerId];
+//       continue;
+//     }
 
-    // Check for collision with other players
-    for (const otherPlayerId in players) {
-      const otherPlayer = players[otherPlayerId];
-      if (
-        otherPlayerId !== playerId &&
-        otherPlayer.x === player.x &&
-        otherPlayer.y === player.y
-      ) {
-        // Game over
-        delete players[playerId];
-        break;
-      }
-    }
-  }
+//     // Check for collision with other players
+//     for (const otherPlayerId in players) {
+//       const otherPlayer = players[otherPlayerId];
+//       if (
+//         otherPlayerId !== playerId &&
+//         otherPlayer.x === player.x &&
+//         otherPlayer.y === player.y
+//       ) {
+//         // Game over
+//         delete players[playerId];
+//         break;
+//       }
+//     }
+//   }
 
-  // Send the updated game state to all players
-  io.emit('updateState', players);
-}, 1000 / 10);
+//   // Send the updated game state to all players
+//   io.emit('updateState', players);
+// }, 1000 / 10);
--- server.js
+++ server.js
@@ -1,49 +1,38 @@
-// const express = require('express');
-// const path = require('path');
-
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-// // Serve static files from the "public" directory
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // Handle requests to the root URL
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// });
-
 const express = require('express');
 const app = express();
-const httpServer = require('http').createServer(app);
-const io = require('socket.io')(httpServer);
+// const httpServer = require('http').createServer(app);
+// const io = require('socket.io')(httpServer);
 
 const port = 3000;
 
-app.use(express.static('public'));
-
-// Handle client connections
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  // Handle messages from the client
-  socket.on('message', (data) => {
-    console.log('Received message:', data);
-
-    // Broadcast the message to all connected clients
-    io.emit('message', data);
-  });
-
-  // Handle disconnections
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
+app.get('/', (req, res) => {
+  res.send("Hello, World!");
 });
 
-httpServer.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, (req, res) => {
+  console.log('Example app listening on port ${port}');
 });
+
+// app.use(express.static('public'));
+
+// // Handle client connections
+// io.on('connection', (socket) => {
+//   console.log('A user connected');
+
+//   // Handle messages from the client
+//   socket.on('message', (data) => {
+//     console.log('Received message:', data);
+
+//     // Broadcast the message to all connected clients
+//     io.emit('message', data);
+//   });
+
+//   // Handle disconnections
+//   socket.on('disconnect', () => {
+//     console.log('A user disconnected');
+//   });
+// });
+
+// httpServer.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
