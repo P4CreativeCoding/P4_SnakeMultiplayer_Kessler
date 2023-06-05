18ff08296553173def28dbb785c5109d45a79e5a
Batmania <ikessler116@gmail.com>
2023-06-05 12:02:12 +0200

    fix server players issue

--- server.js
+++ server.js
@@ -18,9 +18,7 @@ function createNewPlayer(socket){
 };
 
 function deletePlayer(){
-  if(!Array.isArray(players)) { return; }
-  
-  for(var i = this.players.length - 1; i >= 0; i--){
+  for(var i = players.length - 1; i >= 0; i--){
     if(players[i].id == socket.id){
         players.slice(i, 1);
     }
