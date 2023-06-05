94575bbe907e15dd895c4c620f5bb1720d329d89
Batmania <ikessler116@gmail.com>
2023-06-05 13:38:23 +0200

    player movement bounds

--- server.js
+++ server.js
@@ -32,29 +32,68 @@ function deletePlayer(socket){
   delete players[socket.id];
 };
 
+function movePlayer(player){
+  var oldPos = {x: player.x, y: player.y};
+
+  if("up" === player.direction)
+  {
+    player.y -= 1;
+  }
+  else if("down" === player.direction)
+  {
+    player.y += 1;
+  }
+  else if("left" === player.direction)
+  {
+    player.x -= 1;
+  }
+  else if("right" === player.direction)
+  {
+    player.x += 1;
+  }
+};
+
+function collidesWith(a, b){
+  return a.x === b.x && a.y === b.y;
+};
+
+function bitesSelf(player)
+{
+    for (var tailPiece in player.tail)
+    {
+        if(collidesWith(tailPiece, player))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function collidesWithBarrier(player)
+{
+    return (player.x < 0 || player.x > gridSize) || (player.y < 0 || player.y > gridSize);
+}
+
+function resetPlayer(player)
+{
+    player.x = 18;
+    player.y = 18;
+    tail = [];
+    direction = "up";
+}
+
 function updatePlayers(){
   for(var id in players){
     var player = players[id];
     if(!player) { continue; }
 
-    var oldPosX = player.x
-    var oldPosY = player.y;
+    movePlayer(player);
 
-    if("up" === player.direction)
+    if(bitesSelf(player) || collidesWithBarrier(player))
     {
-      player.y -= 1;
-    }
-    else if("down" === player.direction)
-    {
-      player.y += 1;
-    }
-    else if("left" === player.direction)
-    {
-      player.x -= 1;
-    }
-    else if("right" === player.direction)
-    {
-      player.x += 1;
+        resetPlayer(player);
+        return;
     }
   }
 
